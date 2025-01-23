4ff921d7fc97a8474d66469fc50ea1810052ee8d
Ananthu Maheswaran <ananthumaheswaran098@gmail.com>
2025-01-23 23:07:48 +0530

    Refactor: update usersRouter.js

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,13 +1,19 @@
 import { Router } from "express";
-import usersController from "../controllers/usersController.js";
-
+import {
+  usersListGet,
+  usersCreateGet,
+  usersCreatePost,
+  usersUpdateGet,
+  usersUpdatePost,
+  usersDeletePost,
+} from "../controllers/usersController.js";
 const usersRouter = Router();
 
-usersRouter.get("/", usersController.usersListGet);
-usersRouter.get("/create", usersController.usersCreateGet);
-usersRouter.post("/create", usersController.usersCreatePost);
-usersRouter.get("/:id/update", usersController.usersUpdateGet);
-usersRouter.post("/:id/update", usersController.usersUpdatePost);
-usersRouter.post("/:id/delete", usersController.usersDeletePost);
+usersRouter.get("/", usersListGet);
+usersRouter.get("/create", usersCreateGet);
+usersRouter.post("/create", usersCreatePost);
+usersRouter.get("/:id/update", usersUpdateGet);
+usersRouter.post("/:id/update", usersUpdatePost);
+usersRouter.post("/:id/delete", usersDeletePost);
 
 export default usersRouter;
